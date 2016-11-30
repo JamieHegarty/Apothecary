566428a50c038999d19b4714ebaa21d5d4ee4061
Jamie Hegarty <Jamiehegarty@hotmail.com>
2016-11-30 00:37:28 +0000

    This was quite a large refactor of the existing architecture to make it more extendable in future and make sure our classes adhere to the single responsibility protocol - Jamie

---
+++ Assets/Scripts/AnimationDriver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationDriver : MonoBehaviour {
+	private Animator animator;
+
+	void Awake () {
+		animator = GetComponent<Animator> ();
+	}
+
+	public void PlayWalkingAnimation (bool state){
+		animator.SetBool ("IsWalking", state);
+	}
+
+}
